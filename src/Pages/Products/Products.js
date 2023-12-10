3bbd65437c4e6ba66f7fa2df9315a3485a1bcb58
Yatin1119 <yatinchokshi95@gmail.com>
2023-12-11 00:30:25 +0530

    Product page

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import Transitions from "../../Components/Transition/Transition";
-
+import './Products.css';
 export default function Products(){
     useEffect(()=>{
         window.scrollTo(0,0);
@@ -8,7 +8,12 @@ export default function Products(){
     return (
         <div className="container"> 
         <Transitions>
-            <h1>Product page</h1>
+            <div className="product-details">
+                <div className="product-image">
+                    <img src="src/assets/images/Table-img.png"></img>
+                </div>
+                <h1>Tray Table</h1>
+            </div>
         </Transitions>
         </div>
     )
