eca799d378c33b25b218082a28679d416088fa66
Yatin <yatinchokshi95@gmail.com>
2024-01-21 19:59:49 +0530

    authentication

--- src/Pages/SignUp/SignUp.tsx
+++ src/Pages/SignUp/SignUp.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import './SignUp.css';
-import { Button, Checkbox, Grid, Input, TextField, ThemeProvider, createTheme, styled } from '@mui/material';
-import { NavLink } from 'react-router-dom';
-import { FormControl, useFormControlContext } from '@mui/base/FormControl';
+import { Button, Checkbox, FormControl, Grid, Input, TextField, ThemeProvider, createTheme, styled } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
 export default function SignUp( ){
+    type FormValues = {
+        name:string,
+        email:string,
+        password:string
+    }
     const theme = createTheme({
         
         palette: {
@@ -17,25 +23,33 @@ export default function SignUp( ){
     const handleHaveAccount = ()=>{
         sethaveAccount(!haveAccount)
     }
-    const handleFormChange = (e)=>{
-        const { name, value, type, checked } = e.target;
-        console.log(e.target);
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: type === 'checkbox' ? checked : value,
-          }));
+  const form  = useForm<FormValues>({
+    defaultValues:{
+        email:'',
+        name:'',
+        password:''
     }
-    const onSignUp = ()=>{
-        console.log(formData);
-    }
-    const [formData, setFormData] = React.useState({
-    name: '',
-    username: '',
-    email: '',
-    password: '',
-    agreement: false, 
   });
+
+  const {register,handleSubmit,formState}  = form
+  const {errors} = formState 
     const [haveAccount, sethaveAccount] = React.useState(false);
+    const navigate = useNavigate()
+    const onSubmit = (data:FormValues)=>{
+        if(haveAccount){
+            axios.post('https://localhost:7275/api/User/Authenticate',{email:data.email,password:data.password}).then(res=>{
+                if(res.data){
+                    navigate('/');
+                }
+            })
+        }else{
+            axios.post('https://localhost:7275/api/User',{name:data.name, email:data.email,password:data.password}).then(res=>{
+                if(res.data){
+                    navigate('/');
+                }
+            })
+        }
+    }
     return (
      <div>
         <Grid container className='signup'>
@@ -46,29 +60,26 @@ export default function SignUp( ){
             </Grid>
             <Grid item md={6} xs={12} className='signup-form-container'>
                 {!haveAccount?
-                <form className='signup-form'>
+                <form className='signup-form' onSubmit={handleSubmit(onSubmit)}>
                     <h2>Sign Up</h2>
                     <span>Already have an account? <b className='signup-link' onClick={handleHaveAccount}>Sign In</b></span>
                     <ThemeProvider theme={theme}>
-                        <TextField required name="name"  type='text' value={formData.name} onChange={handleFormChange} variant="standard" label="Your name" className='input-field' />
-                        <TextField required name="username" value={formData.username} onChange={handleFormChange} variant="standard" label="User name" className='input-field' />
-                        <TextField required name="email" value={formData.email} onChange={handleFormChange} variant="standard" label="Email Address" />
-                        <TextField required name="password" value={formData.password} onChange={handleFormChange} variant="standard" type='password' label="Password" className='input-field' />
-                        <span><Checkbox required name="agreement" checked={formData.agreement} onChange={handleFormChange}></Checkbox>I agree with <b>Privacy Policy</b> and  <b>Terms of use</b></span>
-                            <Button variant="contained" className="signup-button" type='submit' onClick={onSignUp}> Sign up</Button>
+                        <TextField {...register('name')} error={!!errors.name} helperText={!!errors.name?.message} required name="name"  type='text'   variant="standard" label="Your name" className='input-field' />
+                        <TextField {...register('email')} error={!!errors.email} helperText={!!errors.email?.message} required name="email"   variant="standard" label="Email Address" />
+                        <TextField {...register('password')} error={!!errors.password} helperText={!!errors.password?.message} required name="password"   variant="standard" type='password' label="Password" className='input-field' />
+                        <span><Checkbox required name="agreement"  ></Checkbox>I agree with <b>Privacy Policy</b> and  <b>Terms of use</b></span>
+                            <Button variant="contained" className="signup-button" type='submit'> Sign up</Button>
                     </ThemeProvider>
                  </form>
                 :
-                <form className='signin-form'>
+                <form className='signin-form' onSubmit={handleSubmit(onSubmit)}>
                     <h2>Sign In</h2>
                     <span>Don't have an account yet? <b className='signup-link' onClick={handleHaveAccount}>Sign Up</b></span>
                     <ThemeProvider theme={theme}>
-                    <TextField variant="standard" label="User name or Email" className='input-field' />
-                    <TextField variant="standard" type='password' label="Password" className='input-field' />
+                    <TextField required  {...register('email')} error={!!errors.email}  variant="standard" label="Email"   className='input-field' />
+                    <TextField required   {...register('password')} error={!!errors.password} variant="standard" type='password' label="Password"   className='input-field' />
                     <span><Checkbox></Checkbox>Remember me</span>
-                    <NavLink to='/'>
                         <Button variant="contained" className="signup-button" type='submit'> Sign In</Button>
-                    </NavLink>
                     </ThemeProvider>
                 </form>
                 }
